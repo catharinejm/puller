41698b7f6b94011cac7a261a476a4b0ec1cc5fb3
Jon Distad <jon.distad@gmail.com>
2014-12-21 10:06:32 -0500

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to puller
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/puller/core_test.clj
@@ -0,0 +1,7 @@
+(ns puller.core-test
+  (:require [clojure.test :refer :all]
+            [puller.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
